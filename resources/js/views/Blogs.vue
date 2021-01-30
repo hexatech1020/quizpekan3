db17452babbf7f80a8abbecb2612598285dd744f
unknown <hexatech1020@gmail.com>
2021-01-30 20:03:16 +0700

    first commit

---
+++ resources/js/views/Blogs.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Blogs</div>
+
+                    <div class="card-body">
+                        Ini adalah daftar Blogs
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted(){
+            console.log('component mounted')    
+        }
+    }
+</script>
---
+++ routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group([
+    'middleware' => ['api', 'email_verified', 'auth:api'],
+], function(){
+    Route::get('profile/show', 'ProfileController@show');
+    Route::post('profile/update', 'ProfileController@update');
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'campaign'
+], function(){
+    Route::get('random/{count}', 'CampaignController@random');
+    Route::post('store', 'CampaignController@store');
+    Route::get('/','CampaignController@index');
+    Route::get('/{id}','CampaignController@detail');
+
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'blog'
+], function(){
+    Route::get('random/{count}', 'BlogController@random');
+    Route::post('store', 'BlogController@store');
+});
